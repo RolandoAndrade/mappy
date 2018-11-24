d15bbb50990ddb91d392ddd497504b57cbd252e9
MAEC <moisesucab@gmail.com>
2018-11-24 16:33:33 -0400

    Intento de dashboard + delivery_address form

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import signup, index, login
+from .views import signup, index, login, dashboard
 
 
 urlpatterns = [
     path('signup/', signup, name = "signup"),
     path('login/', login, name = "login"),
+    path('dashboard/', dashboard, name = "dashboard"),
     path('', index, name="index"),
 ]
