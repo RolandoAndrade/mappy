650dba4eb8c000957359b319b00bf3424bd552b9
Luis Alberto <luis.alberto199846@gmail.com>
2018-11-27 21:10:44 -0400

    Agregado formulario de registro de paquete y su URL.

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import signup, index, login
+from .views import signup, index, login, add_package
 
 
 urlpatterns = [
     path('signup/', signup, name = "signup"),
     path('login/', login, name = "login"),
     path('', index, name="index"),
+    path('add_package/', add_package, name="add_package"),
 ]
