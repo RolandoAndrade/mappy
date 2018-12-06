615b97573388088794d3038cb2171c20ea75dcc7
Super Proyectos <rolandoandrade.estudio@gamil.com>
2018-12-06 09:22:50 -0400

    Merge de feature/collection_address_form

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,7 @@ from .views import signup, index, login, addAddress, add_package
 urlpatterns = [
     path('signup/', signup, name = "signup"),
     path('login/', login, name = "login"),
+
     path('add_address/', addAddress, name = "addAddress"),
     path('', index, name="index"),
     path('add_package/', add_package, name="add_package"),
