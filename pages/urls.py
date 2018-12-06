bed17613de35c200fec15e259fb9f3d2a9ed04a3
Rolando Andrade <38079269+superproyectos@users.noreply.github.com>
2018-12-06 09:04:47 -0400

    Revert "Agregado formulario de registro de paquete y su URL."

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import signup, index, login, add_package
+from .views import signup, index, login
 
 
 urlpatterns = [
     path('signup/', signup, name = "signup"),
     path('login/', login, name = "login"),
     path('', index, name="index"),
-    path('add_package/', add_package, name="add_package"),
 ]
